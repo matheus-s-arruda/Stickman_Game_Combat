19b52271646a5641ebcdf50d2b27ba43dbf889f0
matheus arruda <59703897+matheus-s-arruda@users.noreply.github.com>
2021-11-23 21:50:39 -0300

    Create line.gd

---
+++ src/stickman/characters/master_hong/assets/line.gd
@@ -0,0 +1,15 @@
+tool
+extends Position2D
+
+export(int) var _legth := 50
+
+var color := Color(0.6, 0.48, 0)
+
+func _process(delta):
+	update()
+
+
+func _draw():
+	draw_circle(Vector2.ZERO, 5, color)
+	draw_line(Vector2.ZERO, to_local(get_parent().global_position), color, 10)
+
